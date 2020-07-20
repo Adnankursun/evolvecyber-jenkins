df336d7442d86b414aac8d75ce8aeb6722c01432
adnan kursun <54333199+Adnankursun@users.noreply.github.com>
2020-07-20 01:10:25 -0500

    Update install-jenkins-script.sh

--- class4/packer/install-jenkins-script.sh
+++ class4/packer/install-jenkins-script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo yum install epel-release -y
 sudo yum install wget java -y 
-sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
-sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 sudo yum install jenkins -y 
 sudo systemctl start jenkins
 sudo systemctl enable jenkins
